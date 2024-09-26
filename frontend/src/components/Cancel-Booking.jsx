d372a88f1a8b6f8c3d4dd265abf95d4f3222d0fd
tnnsng <6614210017@mut.ac.th>
2024-09-26 13:49:24 +0700

    update-cancel

--- frontend/src/components/Cancel-Booking.jsx
+++ frontend/src/components/Cancel-Booking.jsx
@@ -6,10 +6,83 @@ import {
   FaClock,
 } from "react-icons/fa";
 import "react-datepicker/dist/react-datepicker.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import Swal from "sweetalert2";
+
+const formatDateToUniversal = (date) => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // เดือนต้องบวก 1 เนื่องจาก getMonth() นับจาก 0
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  return `${year}-${month}-${day} ${hours}:${minutes}`;
+};
 
 const CancelBooking = () => {
   const booking = JSON.parse(localStorage.getItem("selectedRoomBooking"));
+  const [reason, setReason] = useState("");
+  const navigate = useNavigate();
+
+  const handleCancel = async () => {
+    try {
+      const now = new Date();
+      const bookDate = formatDateToUniversal(now);
+
+      const response = await fetch(
+        `http://localhost:5000/cancle/${booking.BOOK_ID}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            cancle_date: bookDate,
+            reason: reason,
+            emp_id: booking.EMP_ID,
+          }),
+        }
+      );
+
+      const result = await response.json();
+      console.log(result.reason);
+
+      if (response.ok) {
+        Swal.fire({
+          icon: "success",
+          title: "เสร็จสิ้น",
+          text: "ยกเลิกการจองสำเร็จ",
+        });
+
+        navigate("/main/booking-history");
+      }
+    } catch (error) {
+      console.error("Error booking room:", error);
+      Swal.fire({
+        icon: "error",
+        title: "เกิดข้อผิดพลาด",
+        text: "เกิดข้อผิดพลาดขณะยกเลิกการจองห้องประชุม กรุณาลองใหม่อีกครั้ง",
+      });
+    }
+  };
+
+  const confirmCancel = () => {
+    Swal.fire({
+      title: "คุณต้องยกเลิกการจองห้องประชุมใช่หรือไม่?",
+      html: `ห้อง: ${booking.ROOM_NAME} ตึก: ${booking.BUILD_NAME} ชั้น: ${booking.FLOOR_NAME}`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "ใช่",
+      cancelButtonText: "ยกเลิก",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        handleCancel();
+      }
+    });
+  };
 
   return (
     <div className="p-8 bg-white text-gray-800 min-h-screen">
@@ -27,7 +100,7 @@ const CancelBooking = () => {
           <label className="relative">
             <input
               type="text"
-              className="input input-bordered rounded-xl w-full bg-white drop-shadow-lg text-xl text-gray-800 pl-14"
+              className="input rounded-xl w-full bg-white text-xl text-gray-800 pl-14"
               value={`: ${booking.TYPE_NAME}`}
               readOnly
             />
@@ -39,7 +112,7 @@ const CancelBooking = () => {
           <label className="relative">
             <input
               type="text"
-              className="input input-bordered rounded-xl w-full bg-white drop-shadow-lg text-xl text-gray-800 pl-14"
+              className="input rounded-xl w-full bg-white text-xl text-gray-800 pl-14"
               value={`: ${booking.BUILD_NAME}`}
               readOnly
             />
@@ -51,7 +124,7 @@ const CancelBooking = () => {
           <label className="relative">
             <input
               type="text"
-              className="input input-bordered rounded-xl w-full bg-white drop-shadow-lg text-xl text-gray-800 pl-14"
+              className="input rounded-xl w-full bg-white text-xl text-gray-800 pl-14"
               value={`: ${booking.ROOM_NAME}`}
               readOnly
             />
@@ -63,7 +136,7 @@ const CancelBooking = () => {
           <label className="relative">
             <input
               type="text"
-              className="input input-bordered rounded-xl w-full bg-white drop-shadow-lg text-xl text-gray-800 pl-14"
+              className="input rounded-xl w-full bg-white text-xl text-gray-800 pl-14"
               value={`: ${booking.STARTDATE.split(" ")[1]} - ${
                 booking.ENDDATE.split(" ")[1]
               }`}
@@ -71,6 +144,27 @@ const CancelBooking = () => {
             />
             <FaClock className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-800 text-4xl" />
           </label>
+        </div>
+
+        <div className="flex items-center mb-6">
+          <label className="relative w-full">
+            <h2 className="text-lg mb-2">เหตุผลในการยกเลิก</h2>
+            <input
+              type="text"
+              className="input input-bordered border-gray-400 rounded-xl w-1/3 bg-white drop-shadow-lg text-xl text-gray-800 pl-4"
+              value={reason}
+              onChange={(e) => setReason(e.target.value)}
+            />
+          </label>
+        </div>
+
+        <div className="flex justify-end">
+          <button
+            className="bg-red-900 text-xl text-center text-white py-3 px-6 rounded-xl hover:bg-red-950 drop-shadow-xl"
+            onClick={confirmCancel}
+          >
+            ยกเลิกการจอง
+          </button>
         </div>
       </div>
     </div>
